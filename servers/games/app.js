7c2ecb7d7962d6539cc39c0f6844ced83514db77
Spencer White <spencer.white@carat.com>
2019-01-08 14:21:29 -0500

    implementing express request id and morgan logger middlewares

--- servers/games/app.js
+++ servers/games/app.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
+const expressRequestID = require("express-request-id")();
+const requestLogger = require("../shared/lib/requestLogger");
 
 const app = express();
 
-app.set('x-powered-by', false);
+app.set("x-powered-by", false);
+app.use(expressRequestID);
 
 app.use(express.json());
 
-app.use(require('./router'));
+app.use(requestLogger);
+app.use(require("./router"));
 
 module.exports = app;
--- servers/players/app.js
+++ servers/players/app.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
+const requestLogger = require("../shared/lib/requestLogger");
+const expressRequestID = require("express-request-id")();
 
 const app = express();
 
-app.set('x-powered-by', false);
+app.set("x-powered-by", false);
 
 app.use(express.json());
-
-app.use(require('./router'));
+app.use(expressRequestID);
+app.use(requestLogger);
+app.use(require("./router"));
 
 module.exports = app;
