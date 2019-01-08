365775834839bfe7fefc56eb8bd606a2672a6bd8
Spencer White <spencer.white@carat.com>
2019-01-08 13:07:20 -0500

    logger files

--- servers/shared/lib/logger.js
+++ servers/shared/lib/logger.js
@@ -1,12 +1,7 @@
 const winston = require("winston");
 
 const logger = winston.createLogger({
-    transports: [
-        new winston.transports.Console({
-            timestamp: new Date().toISOString(),
-            colorize: true
-        })
-    ]
+    transports: [new winston.transports.Console()]
 });
 
 module.exports = logger;
