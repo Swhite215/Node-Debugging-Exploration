e270ff1c23f4146c3764622868788723d7e9dd1b
Spencer White <spencer.white@carat.com>
2019-01-08 13:41:36 -0500

    Notes on morgan http request logger using winston

--- servers/shared/lib/requestLogger.js
+++ servers/shared/lib/requestLogger.js
@@ -1,3 +1,13 @@
 const morgan = require("morgan");
+const logger = require("./logger");
 
-module.exports = morgan("dev");
+const format = `:requestId :method :url :status :response-time ms`;
+
+morgan.token("requestId", request => request.id);
+
+const options = {
+    stream: {
+        write: message => logger.info(message.trim())
+    }
+};
+module.exports = morgan(format, options);
